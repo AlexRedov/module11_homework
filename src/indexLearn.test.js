2f6f1b1b9c2237f51768b7322e22c26080d5ab9b
Александр Краснов <kralex87@mail.ru>
2022-02-08 19:23:02 +0300

    reinstall babel and change intervalNum function test

--- src/indexLearn.test.js
+++ src/indexLearn.test.js
@@ -1,21 +1,21 @@
-import { intervalNum } from "../../utils/indexLearn.js";
+import { intervalNum } from "src/indexLearn.js";
 
-describe ("tests for definePrime function", () => {
+describe ("tests for intervalNum function", () => {
     
   
     xit("should operate correctly with pozitive number", () => {
-      expect(definePrime(10,15)).toBe(`10,11,12,13,14,15`);
+      expect(intervalNum(10,15)).toBe(`10,11,12,13,14,15`);
     }),
 
     it("should operate correctly with negative number", () => {
-        expect(definePrime(-10,-7)).toBe(`-10,-9,-8,-7`);
+        expect(intervalNum(-10,-7)).toBe(`-10,-9,-8,-7`);
     }),
 
     it("should operate correctly with same number", () => {
-        expect(definePrime(10,10)).toBe(`10`);
+        expect(intervalNum(10,10)).toBe(`10`);
     }),
 
     it("should operate uncorrectly with this number", () => {
-        expect(definePrime(1,0)).toBe(`10`);
+        expect(intervalNum(1,0)).toBe(`10`);
     })
 })
